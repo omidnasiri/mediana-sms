2ad0398b68429545e508b30fc141755e339e9fdd
Omid Nasiri <omid_nasiriii@outlook.com>
2023-08-23 21:11:21 +0330

    connect to postgres

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,5 +1,30 @@
 package db
 
-func Migrate() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+// Migrate opens the postgres database connection
+// and migrates the database schema
+func Migrate() *gorm.DB {
+	host := os.Getenv("POSTGRES_HOST")
+	port := os.Getenv("POSTGRES_PORT")
+	name := os.Getenv("POSTGRES_NAME")
+	username := os.Getenv("POSTGRES_USERNAME")
+	password := os.Getenv("POSTGRES_PASSWORD")
+
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%v/%s", username, password, host, port, name)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db.AutoMigrate()
+
+	return db
 }
